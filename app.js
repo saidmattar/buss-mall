533840cd65483c4eadef80fa4cc12706a3b6b409
saidmattar <ssabdou@gmail.com>
2017-04-17 20:27:32 -0700

    added three pictures to the main page

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 'use strict';
+var mainDiv = document.getElementById('clickable-images');
+var firstImage =document.createElement('img');
+mainDiv.appendChild(firstImage);
+firstImage.src = 'img/bag.jpg';
+console.log(firstImage);
+var secondImage =document.createElement('img');
+mainDiv.appendChild(secondImage);
+secondImage.src ='img/banana.jpg';
+var thirdImage = document.createElement('img');
+mainDiv.appendChild(thirdImage);
+thirdImage.src = 'img/chair.jpg';
+
+
+
+
+
+
+
+
+
+
+
+
+//var count = 0;
+// var countClicks= document.getElementById('clicks-first');
+// countClicks.textContent = count;
+//
+// function handleIncButtonClick(){
+//   console.log('clicking!');
+//   count++;
+//   countClicks.textContent = count;
+// }
+// countClicks.addEventListener('click', handleIncButtonClick);
